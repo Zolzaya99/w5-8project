177833cc607bee1cd51303df7855081efb716065
Zolzaya99 <zolzayaoyunkhand@gmail.com>
2023-11-01 18:20:48 -0600

    extra folder deleted.

--- controllers/authenticate.js
+++
@@ -1,9 +0,0 @@
-const logInandOut = (req, res) => {
-    res.send(
-        req.oidc.isAuthenticated()
-        ? `Logged In as ${req.oidc.user.name}`
-        : 'Logged Out'
-    );
-};
-
-module.exports = { logInandOut };
